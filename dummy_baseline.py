0e5baa995bd26acd3d465129b24df5cdf4abf392
lucasgascon <76687633+lucasgascon@users.noreply.github.com>
2022-11-07 22:26:22 +0100

    commit

--- dummy_baseline.py
+++ dummy_baseline.py
@@ -1,3 +1,5 @@
+# %%
+
 import csv
 import numpy as np
 import pandas as pd
@@ -8,17 +10,19 @@ from sklearn.metrics import mean_absolute_error
 from verstack.stratified_continuous_split import scsplit # pip install verstack
 from nltk.corpus import stopwords 
 
+#%%
 # Load the training data
 train_data = pd.read_csv("train.csv")
 eval_data = pd.read_csv("evaluation.csv")
 
+#%%
 # Transform our data into tfidf vectors
 vectorizer = TfidfVectorizer(max_features=100, stop_words=stopwords.words('french'))
 y_train = train_data['retweets_count']
 X_train = vectorizer.fit_transform(train_data['text'])
 # We fit our model using the training data
 
-
+#%%
 # A dummy regressor that always predicts the mean value of the training data
 dummy_regr = DummyRegressor(strategy="mean")
 dummy_regr.fit(X_train, y_train)
@@ -29,6 +33,8 @@ with open("mean_predictions.txt", 'w') as f:
     for index, prediction in enumerate(dummy_pred):
         writer.writerow([str(eval_data['TweetID'].iloc[index]) , str(int(prediction))])
 
+
+#%%
 # A dummy regressor that always predicts "0" retweets
 dummy_regr = DummyRegressor(strategy="constant", constant=0)
 dummy_regr.fit(X_train, y_train)
