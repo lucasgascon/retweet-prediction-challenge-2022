a63d88db867b5f5c9de83534d5e998b13d316db4
lucasgascon <76687633+lucasgascon@users.noreply.github.com>
2022-12-07 13:48:11 +0100

    commit

--- model_rfr.py
+++ model_rfr.py
@@ -5,15 +5,16 @@ import pickle
 from sklearn.metrics import mean_absolute_error
 from lightgbm import LGBMRegressor
 from utils import load_data, load_data_numpy
+import numpy as np
 
 # MAE error: 6.49
-# X, y, X_train, y_train, X_test, y_test, X_val = load_data('preprocessing')
+X, y, X_train, y_train, X_test, y_test, X_val = load_data('preprocessing')
 
 # MAE error: 6.49
 # X, y, X_train, y_train, X_test, y_test, X_val = load_data_numpy('preprocessing_stscaler')
 
 # MAE error: 6.09
-X, y, X_train, y_train, X_test, y_test, X_val = load_data('old_csv') 
+# X, y, X_train, y_train, X_test, y_test, X_val = load_data('old_csv') 
 
 # MAE error: 6.96
 # X, y, X_train, y_train, X_test, y_test, X_val = load_data('csv150')
@@ -21,7 +22,7 @@ X, y, X_train, y_train, X_test, y_test, X_val = load_data('old_csv')
 # MAE error: 7.46
 # X, y, X_train, y_train, X_test, y_test, X_val = load_data('csv50')
 
-def custom_model(X_train, y_train, X_test, save= False):
+def custom_model(X_train, y_train, X_test, y_test = None, save= False):
     start_time = time.time()
 
     rfc = RandomForestClassifier(
@@ -33,16 +34,30 @@ def custom_model(X_train, y_train, X_test, save= False):
         verbose = 5,
         )
 
-    y_train_2 = y_train['retweets_count'].apply(lambda x : 1 if (x>0) else 0)
+    y_train_2 = y_train['retweets_count'].apply(lambda x : 2 if (x>15) else 1 if (x>7) else 0)
     rfc.fit(X_train, y_train_2)
+    classifier_train = rfc.predict(X_train)
     classifier_test = rfc.predict(X_test)
     
-    # save randomforest classifier
-    if save:
-        filename = './model/randomforest_classifier_1.sav'
-        pickle.dump(rfc, open(filename, 'wb'))
+    X_train_2 = X_train[classifier_train == 2]
+    y_train_2 = y_train[classifier_train == 2]
 
-    reg = LGBMRegressor(
+    X_train_1 = X_train[classifier_train == 1]
+    y_train_1 = y_train[classifier_train == 1]
+
+    X_train_0 = X_train[classifier_train == 0]
+    y_train_0 = y_train[classifier_train == 0]
+
+    X_test_2 = X_test[classifier_test == 2]
+    X_test_1 = X_test[classifier_test == 1]
+    X_test_0 = X_test[classifier_test == 0]
+
+    if y_test != None:
+        y_test_2 = y_test[classifier_test == 2]
+        y_test_1 = y_test[classifier_test == 1]
+        y_test_0 = y_test[classifier_test == 0]
+
+    reg2 = LGBMRegressor(
         boosting_type='gbdt',
         learning_rate = 0.037,
         n_estimators = 300,
@@ -50,22 +65,44 @@ def custom_model(X_train, y_train, X_test, save= False):
         random_state = 42, 
         verbose = 5,
     )
-    
-    reg.fit(X_train, y_train)
+    reg2.fit(X_train_2, y_train_2)
     elapsed_time = time.time() - start_time
     print("took {} seconds for fitting".format(elapsed_time))
+    y_pred_2 = reg2.predict(X_test_2)
 
-    # save LGBM regressor
-    if save:
-        filename = './model/lgbmr_1.sav'
-        pickle.dump(reg, open(filename, 'wb'))
-    
-    y_pred = reg.predict(X_test)
-    y_pred = [y_pred[i]*classifier_test[i] for i in range(len(y_pred))]
+    reg1 = LGBMRegressor(
+        boosting_type='gbdt',
+        learning_rate = 0.037,
+        n_estimators = 300,
+        n_jobs = -1,
+        random_state = 42, 
+        verbose = 5,
+    )
+    reg1.fit(X_train_1, y_train_1)
+    elapsed_time = time.time() - start_time
+    print("took {} seconds for fitting".format(elapsed_time))
+    y_pred_1 = reg1.predict(X_test_1)
+
+    reg0 = LGBMRegressor(
+        boosting_type='gbdt',
+        learning_rate = 0.037,
+        n_estimators = 300,
+        n_jobs = -1,
+        random_state = 42, 
+        verbose = 5,
+    )
+    reg0.fit(X_train_0, y_train_0)
+    elapsed_time = time.time() - start_time
+    print("took {} seconds for fitting".format(elapsed_time))
+    y_pred_0 = reg0.predict(X_test_0)
+
+    y_pred = np.concatenate((y_pred_2, y_pred_1, y_pred_0), axis=None)
     y_pred = [int(value) if value >= 0 else 0 for value in y_pred]
-
+    if y_test != None :
+        y_test = np.concatenate((y_test_2, y_test_1, y_test_0), axis=None)
+        print("Prediction error:", mean_absolute_error(y_true=y_test, y_pred=y_pred))
+    
     return y_pred
-
 
 # MAE error: 6.57
 # X, y, X_train, y_train, X_test, y_test, X_val = load_data_numpy('preprocessing_stscaler')
@@ -109,8 +146,10 @@ def rfr(X_train, y_train, X_test, save= False):
 
     return y_pred
 
-y_pred = rfr(X_train, y_train, X_test, save = False)
-# y_pred = custom_model(X_train, y_train, X_test, save = False)
+# y_pred = rfr(X_train, y_train, X_test, save = False)
+y_pred = custom_model(X_train, y_train, X_test, save = False)
+
+#%%
 print("Prediction error:", mean_absolute_error(y_true=y_test, y_pred=y_pred))
 
 
--- trainSacha.py
+++ trainSacha.py
@@ -6,15 +6,39 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import Lasso
 import pandas as pd
-# from model_rfr import train_custom_model
-
+from model_rfr import custom_model
+from utils import load_data
+import time
+from catboost import CatBoostRegressor
+import pickle
 
 
 dir = 'csv'
-X_train = pd.read_csv('data/' + dir + '/X_train.csv')
-X_test = pd.read_csv('data/' + dir + '/X_test.csv')
-y_train = pd.read_csv('data/' + dir + '/y_train.csv')
-y_test = pd.read_csv('data/' + dir + '/y_test.csv')
+X, y, X_train, y_train, X_test, y_test, X_val = load_data('preprocessing')
+
+
+def model(X_train, y_train, X_test, save= False):
+    start_time = time.time()
+
+    reg = CatBoostRegressor(
+        iterations = 10000,
+        random_state = 42,
+        )
+    
+    reg.fit(X_train, y_train)
+    elapsed_time = time.time() - start_time
+    print("took {} seconds for fitting".format(elapsed_time))
+
+    # save randomforest regressor
+    if save:
+        filename = 'model/rfc_1.sav'
+        pickle.dump(reg, open(filename, 'wb'))
+    
+    y_pred = reg.predict(X_test)
+    y_pred = [int(value) if value >= 0 else 0 for value in y_pred]
+    
+
+    return y_pred
 
 
 # %%
@@ -37,50 +61,49 @@ y_test_bracket2 = y_test[(X_test['verified']==0) & (X_test['favorites_count']<10
 X_test_bracket3 = X_test[(X_test['verified']==0) & (X_test['favorites_count']>=100)]
 y_test_bracket3 = y_test[(X_test['verified']==0) & (X_test['favorites_count']>=100)]
 
-y_train_bracket2 = np.array(y_train_bracket2['retweets_count'])
-y_test_bracket2 = np.array(y_test_bracket2['retweets_count'])
+# y_train_bracket2 = np.array(y_train_bracket2['retweets_count'])
+# y_test_bracket2 = np.array(y_test_bracket2['retweets_count'])
 
-y_train_bracket1 = np.array(y_train_bracket1['retweets_count'])
-y_test_bracket1 = np.array(y_test_bracket1['retweets_count'])
+# y_train_bracket1 = np.array(y_train_bracket1['retweets_count'])
+# y_test_bracket1 = np.array(y_test_bracket1['retweets_count'])
 
-y_train_bracket3 = np.array(y_train_bracket3['retweets_count'])
-y_test_bracket3 = np.array(y_test_bracket3['retweets_count'])
+# y_train_bracket3 = np.array(y_train_bracket3['retweets_count'])
+# y_test_bracket3 = np.array(y_test_bracket3['retweets_count'])
 
 
-#%%
-# X_train = pd.read_csv('data/csv/X_train.csv', index_col = 0)
-# X_test = pd.read_csv('data/csv/X_test.csv', index_col = 0)
-# y_train = pd.read_csv('data/csv/y_train.csv', index_col=0)
-# y_test = pd.read_csv('data/csv/y_test.csv', index_col=0)
-# %%
-# reg = train_custom_model(X_train, y_train)
-
-dt_reg = DecisionTreeRegressor(random_state=0, criterion='friedman_mse', ccp_alpha=0.1)
-dt_reg.fit(X_train_bracket2, y_train_bracket2)
-y_pred_dt = dt_reg.predict(X_test_bracket2)
-y_pred = [int(value) if value >= 0 else 0 for value in y_pred_dt]
-print("Prediction error:", mean_absolute_error(y_true=y_test_bracket2, y_pred=y_pred))
 
 # %%
-dt_reg3 = DecisionTreeRegressor(random_state=0, criterion='friedman_mse', ccp_alpha=0.1)
-dt_reg3.fit(X_train_bracket3, y_train_bracket3)
-y_pred_dt = dt_reg3.predict(X_test_bracket3)
-y_pred = [int(value) if value >= 0 else 0 for value in y_pred_dt]
-print("Prediction error:", mean_absolute_error(y_true=y_test_bracket3, y_pred=y_pred))
+
+y_pred_dt_2 = model(X_train_bracket2, y_train_bracket2, X_test_bracket2)
+# dt_reg = DecisionTreeRegressor(random_state=0, criterion='friedman_mse', ccp_alpha=0.1)
+# dt_reg.fit(X_train_bracket2, y_train_bracket2)
+# y_pred_dt = dt_reg.predict(X_test_bracket2)
+# y_pred = [int(value) if value >= 0 else 0 for value in y_pred_dt]
+print("Prediction error:", mean_absolute_error(y_true=y_test_bracket2, y_pred=y_pred_dt_2))
+
+# %%
+y_pred_dt_3 = model(X_train_bracket3, y_train_bracket3, X_test_bracket3)
+# dt_reg3 = DecisionTreeRegressor(random_state=0, criterion='friedman_mse', ccp_alpha=0.1)
+# dt_reg3.fit(X_train_bracket3, y_train_bracket3)
+# y_pred_dt = dt_reg3.predict(X_test_bracket3)
+# y_pred = [int(value) if value >= 0 else 0 for value in y_pred_dt]
+print("Prediction error:", mean_absolute_error(y_true=y_test_bracket3, y_pred=y_pred_dt_3))
    
-dt_reg1 = DecisionTreeRegressor(random_state=0, criterion='friedman_mse', ccp_alpha=0.1)
-dt_reg1.fit(X_train_bracket1, y_train_bracket1)
-y_pred_dt = dt_reg1.predict(X_test_bracket1)
-y_pred = [int(value) if value >= 0 else 0 for value in y_pred_dt]
-print("Prediction error:", mean_absolute_error(y_true=y_test_bracket1, y_pred=y_pred))
+y_pred_dt_1 = model(X_train_bracket1, y_train_bracket1, X_test_bracket1)
+# dt_reg1 = DecisionTreeRegressor(random_state=0, criterion='friedman_mse', ccp_alpha=0.1)
+# dt_reg1.fit(X_train_bracket1, y_train_bracket1)
+# y_pred_dt = dt_reg1.predict(X_test_bracket1)
+# y_pred = [int(value) if value >= 0 else 0 for value in y_pred_dt]
+print("Prediction error:", mean_absolute_error(y_true=y_test_bracket1, y_pred=y_pred_dt_1))
    
 # %%
-y_pred_dt_1 = dt_reg1.predict(X_test_bracket1)
-y_pred_1 = [int(value) if value >= 0 else 0 for value in y_pred_dt_1]
-y_pred_dt_2 = dt_reg.predict(X_test_bracket2)
-y_pred_2 = [int(value) if value >= 0 else 0 for value in y_pred_dt_2]
-y_pred_dt_3 = dt_reg3.predict(X_test_bracket3)
-y_pred_3 = [int(value) if value >= 0 else 0 for value in y_pred_dt_3]
+# y_pred_dt_1 = dt_reg1.predict(X_test_bracket1)
+# y_pred_1 = [int(value) if value >= 0 else 0 for value in y_pred_dt_1]
+# y_pred_dt_2 = dt_reg.predict(X_test_bracket2)
+# y_pred_2 = [int(value) if value >= 0 else 0 for value in y_pred_dt_2]
+# y_pred_dt_3 = dt_reg3.predict(X_test_bracket3)
+# y_pred_3 = [int(value) if value >= 0 else 0 for value in y_pred_dt_3]
+
 
 # %%
 
